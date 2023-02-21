41d7cc99796866f8a48d868c79ded4922afb085b
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-21 23:46:34 +0530

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int age = 18;
+    int* ptr = &age;
+    cout<<age<<'\n';
+    cout<<ptr<<'\n';
+    cout<<&age<<'\n';
+    
+}
+
+/*
+OP -->
+18
+0x7fff4818e47c
+0x7fff4818e47c
+
+*/
