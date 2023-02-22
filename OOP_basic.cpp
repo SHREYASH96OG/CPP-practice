125fd455e91f6b50a6b1dcfae3865c1217e36462
SHREYASH GAIKWAD <96031865+SHREYASH96OG@users.noreply.github.com>
2023-02-22 20:03:54 +0530

    Create OOP_basic.cpp

---
+++ OOP_basic.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
+
+int main() {
+  MyClass myObj;  // Create an object of MyClass
+
+  // Access attributes and set values
+  myObj.myNum = 15; 
+  myObj.myString = "Some text";
+
+  // Print attribute values
+  cout << myObj.myNum << "\n";
+  cout << myObj.myString;
+  return 0;
+}
+
+/*
+O/P---->
+15
+Some text
+*/
