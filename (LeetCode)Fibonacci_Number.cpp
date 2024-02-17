6dab77d23e896d3418178f497fdfba2c4bc065e5
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2024-02-17 23:09:59 +0530

    Update (LeetCode)Fibonacci_Number.cpp

--- (LeetCode)Fibonacci_Number.cpp
+++ (LeetCode)Fibonacci_Number.cpp
@@ -32,19 +32,11 @@ class Solution {
 public:
     int fib(int n) {
         
-        if(n==0)
-            return 0;
-        if (n==1)
-            return 1;
-        int a=0,b=1,c=0;
-        
-        for(int i=1;i<n;i++)
-        {
-            c=a+b;
-            a=b;
-            b=c;
+        if(n == 0||n == 1){
+            return n;
+        }else{
+            return fib(n -1)+fib(n-2);
         }
-        return c;
         
     
         
